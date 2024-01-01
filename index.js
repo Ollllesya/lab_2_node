114bc42ab3a11e93d3074f45823daf479e4c0015
Ollllesya <os@starta.one>
2024-01-01 14:39:10 +0200

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { router } from './router.js';
 import { createHttpServer } from './http-server.js';
 import { getController, postController, optionsController } from './controller.js';
 
-const PORT = 3000;
+const PORT = 3001;
 
 router.addRoute('/', 'GET', getController);
 router.addRoute('/example', 'POST', postController);
