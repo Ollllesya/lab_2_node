45eb6b93daca4735fd02a10e93792607559c187f
Ollllesya <os@starta.one>
2024-01-01 14:32:52 +0200

    finish lab2

---
+++ controller.js
@@ -0,0 +1,15 @@
+export const getController = (req, res, payload) => {
+    res.setHeader('content-type', 'application/json');
+    res.end(JSON.stringify({ message: `Hello ${req.socket.remoteAddress}`, payload }));
+  };
+  
+  export const postController = (req, res, payload) => {
+    res.setHeader('content-type', 'application/json');
+    res.end(JSON.stringify({ user: payload }));
+  };
+  
+  export const optionsController = (req, res) => {
+    res.setHeader('Allow', 'OPTIONS, GET, POST');
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('OK');
+  };
